d46d29b2384d88f8f28410499b3460e9fff3fadf
Ian Gerolamo <iangerolamo@gmail.com>
2022-05-04 20:16:31 -0300

    Implementando chamada de autenticacao

---
+++ src/app/core/auth/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {tap} from 'rxjs/operators';
+import {User} from "../user/user";
+
+const API_URL = 'http://localhost:3000/userInfo';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http: HttpClient,
+  ) { }
+
+  // authenticate(userName: string, password: string) {
+  //
+  //   return this.http
+  //     .post(
+  //       API_URL + '/user/login',
+  //       { userName, password },
+  //       { observe: 'response'}
+  //     )
+  //     .pipe(tap(res => {
+  //         const authToken = res.headers.get('x-access-token');
+  //         this.userService.setToken(authToken);
+  //         console.log(`User ${userName} authenticated with token ${authToken}`);
+  //       })
+  //
+  //     );
+  // }
+
+  getInfoUserMock() {
+    return this.http.get<User>(API_URL)
+      .pipe(tap(res => console.log(res)));
+  }
+}
